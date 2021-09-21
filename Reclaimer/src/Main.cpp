194bb2c6ab34ad96a9f05948d0ffa9334603837f
LazyPanda07 <semengricenko@gmail.com>
2021-09-21 12:24:02 +0500

    Init commit

---
+++ Reclaimer/src/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "BaseIOSocketStream.h"
+#include "HTTPSNetwork.h"
+#include "JSONParser.h"
+#include "HTTPBuilder.h"
+#include "HTTPParser.h"
+
+#pragma comment (lib, "Networks.lib")
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+
+
+
+	return 0;
+}
